4443086514b9299b06381404dd0e1568d4b629de
Dew <Dew@LAPTOP-COH4JFBF>
2023-09-13 18:16:13 +0530

    Add project files.

---
+++ Services/HomeService.cs
@@ -0,0 +1,31 @@
+﻿namespace DependencyInjectionScope.Services
+{
+    public class HomeService : ITransientService, IScopedService, ISingletonService
+    {
+        Guid id;
+        public HomeService()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid GetID()
+        {
+            return id;
+        }
+    }
+
+    public interface ITransientService
+    {
+        Guid GetID();
+    }
+
+    public interface IScopedService
+    {
+        Guid GetID();
+    }
+
+    public interface ISingletonService
+    {
+        Guid GetID();
+    }
+}
